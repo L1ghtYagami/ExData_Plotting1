6c5163f2f90274a9444cafe437ef684d978cae2c
L1ghtYagami <aneeshp1994@gmail.com>
2020-12-10 17:52:46 +0530

    Code to create plot1.png

---
+++ plot1.R
@@ -0,0 +1,15 @@
+# Source the code to load data and change class of Date and Time columns
+source("load_and_process_data.R")
+
+# Load in the data
+df <- load_data()
+
+# Open the png graphics device
+png("plot1.png")
+
+# Plot the graph
+hist(df$Global_active_power, col = "red", main = "Global Active Power",
+     xlab = "Global Active Power (kilowatts)")
+
+# Close the connection to save the file
+dev.off()
