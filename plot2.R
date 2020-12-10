0fd26abeaf80a843f5696e29d405059b2848b5b4
L1ghtYagami <aneeshp1994@gmail.com>
2020-12-10 17:53:14 +0530

    Code to create plot2.png

---
+++ plot2.R
@@ -0,0 +1,20 @@
+# Source the code to load data and change class of Date and Time columns
+source("load_and_process_data.R")
+
+# Load in the data
+df <- load_data()
+
+# Create Date.Time column in the dataframe
+df <- modify_df(df)
+
+# Open the png graphics device
+png("plot2.png")
+
+# Plot the graph. Specifying type="n" so that no point gets plotted
+plot(df$Date.Time, df$Global_active_power, type = "n",
+     xlab = "", ylab = "Global Active Power (kilowatts)")
+# Add lines without points
+lines(df$Date.Time, df$Global_active_power, type = "l")
+
+# Close the connection to save the file
+dev.off()
